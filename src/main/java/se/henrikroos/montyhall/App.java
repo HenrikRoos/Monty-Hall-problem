858b10af6e2f63f9c1b4759733eed4f3fb90db5d
HenrikRoos <henrik@afternoon.se>
2013-08-10 09:52:26 +0200

    Java App pom

---
+++ src/main/java/se/henrikroos/montyhall/App.java
@@ -0,0 +1,13 @@
+package se.henrikroos.montyhall;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
